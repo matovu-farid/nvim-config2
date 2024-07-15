3c02d8658b972e4a90df0015f97436d9701b8b1c
matovu-farid <matovu90@gmail.com>
2024-07-16 00:42:26 +0300

    Add more mappings

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -1,9 +1,12 @@
 require "nvchad.mappings"
 require "mappings.git"
 require "mappings.treesitter"
+require "mappings.treesitter"
+
 
 local scratch = require "scratch"
 local telescopeBuiltins = require "telescope.builtin"
+local whichKey = require("which-key")
 
 local map = vim.keymap.set
 local input = vim.ui.input
@@ -12,7 +15,6 @@ map("n", "<leader>rt", telescopeBuiltins.resume, { desc = "Resume last telescope
 map("n", "<leader>h", telescopeBuiltins.help_tags, { desc = "Search help tags" })
 
 -- reset keymaps
-vim.keymap.del("n",  "<leader>e")
 -- map("n", "<leader>e", function()
 --   scratch.open({ bufname = "scratch" })
 -- end, { desc = "Open scratch buffer" })
@@ -30,13 +32,18 @@ map("n", "<leader>x", ":source %<CR>", { desc = "Resource" })
 map("n", "<leader>rn", function()
   input({ prompt = "New name: " }, vim.lsp.buf.rename)
 end, { desc = "Resource" })
-map("n", "<leader>gs", ":G stash", { desc = "Git stash" })
-map("n", "<leader>sne", function()
-  require("scissors").editSnippet()
-end)
-map({ "n", "x" }, "<leader>sna", function()
-  require("scissors").addNewSnippet()
-end)
+whichKey.register({
+  ["<leader>sn"] = {
+    name = "Snippet",
+    a = { function()
+      require("scissors").addNewSnippet()
+    end, "Add new snippet" },
+    e = { function()
+      require("scissors").editSnippet()
+    end, "Edit snippet" },
+
+  },
+})
 map("n", "]q", ":cnext<CR>", { desc = "Next quickfix" })
 map("n", "[q", ":cprev<CR>", { desc = "Previous quickfix" })
 map("n", "]e", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
@@ -53,6 +60,7 @@ map(
   ":Telescope neovim-project discover<CR>",
   { silent = true, noremap = true, desc = "Find project" }
 )
+
 map(
   "n",
   "<leader>fh",
@@ -102,3 +110,39 @@ map("i", "<C-y>", 'copilot#Accept("\\<CR>")', {
   replace_keycodes = false,
 })
 vim.g.copilot_no_tab_map = true
+
+
+local cmp = require "cmp"
+
+cmp.setup {
+  mapping = {
+    ["<C-j>"] = cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Select },
+
+    ["<C-k>"] = cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Select },
+
+  },
+}
+local ls = require("luasnip")
+map({"i", "s"}, "<C-]>", function() ls.jump( 1) end, {silent = true})
+map({"i","s"}, "<C-[>", function() ls.jump(-1) end, {silent = true})
+
+whichKey.register({
+    c = {
+        name = "AI/Codeactions",
+      h = { "<cmd>ChatGPT<CR>", "ChatGPT" },
+      e = { "<cmd>ChatGPTEditWithInstruction<CR>", "Edit with instruction", mode = { "n", "v" } },
+      g = { "<cmd>ChatGPTRun grammar_correction<CR>", "Grammar Correction", mode = { "n", "v" } },
+      t = { "<cmd>ChatGPTRun translate<CR>", "Translate", mode = { "n", "v" } },
+      k = { "<cmd>ChatGPTRun keywords<CR>", "Keywords", mode = { "n", "v" } },
+      d = { "<cmd>ChatGPTRun docstring<CR>", "Docstring", mode = { "n", "v" } },
+      u = { "<cmd>ChatGPTRun add_tests<CR>", "Add Tests", mode = { "n", "v" } },
+      o = { "<cmd>ChatGPTRun optimize_code<CR>", "Optimize Code", mode = { "n", "v" } },
+      s = { "<cmd>ChatGPTRun summarize<CR>", "Summarize", mode = { "n", "v" } },
+      f = { "<cmd>ChatGPTRun fix_bugs<CR>", "Fix Bugs", mode = { "n", "v" } },
+      x = { "<cmd>ChatGPTRun explain_code<CR>", "Explain Code", mode = { "n", "v" } },
+      r = { "<cmd>ChatGPTRun roxygen_edit<CR>", "Roxygen Edit", mode = { "n", "v" } },
+      l = { "<cmd>ChatGPTRun code_readability_analysis<CR>", "Code Readability Analysis", mode = { "n", "v" } },
+    }
+},  { prefix = "<leader>" })
+
+map('n', '<leader>cp', ':Copilot panel<CR>', { desc = "Open Copilot panel" })
