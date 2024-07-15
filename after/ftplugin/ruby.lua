1b176f49d30b99e7799fbf1f56f6a5ad4956f6e3
matovu-farid <matovu90@gmail.com>
2024-07-16 00:38:58 +0300

    Add spacing to the rails commands

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -1,5 +1,6 @@
 local map = vim.keymap.set
 
-map("n", "<leader>em",":Emodel", { desc = "Edit model" })
-map("n", "<leader>ev",":Eview", { desc = "Edit view" })
-map("n", "<leader>ec",":Econtroller", { desc = "Edit controller" })
+map("n", "<leader>em",":Emodel ", { desc = "Edit model" })
+map("n", "<leader>ev",":Eview ", { desc = "Edit view" })
+map("n", "<leader>ec",":Econtroller ", { desc = "Edit controller" })
+
