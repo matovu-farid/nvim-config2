2fd75b107a8d7e4076d42f6ab3e0b73d649ecc43
matovu-farid <matovu90@gmail.com>
2024-07-16 00:40:14 +0300

    remove keymappings

--- lua/configs/override.lua
+++ lua/configs/override.lua
@@ -2,5 +2,11 @@ local code = require "luasnip.loaders.from_vscode"
 code.lazy_load { paths = { vim.fn.stdpath "config" .. "/snippets" } }
 require("colorizer").setup { mode = "virtualtext" }
 
-require("configs.telescope")
+require "configs.telescope"
+
+pcall(function()
+  vim.keymap.del("n", "<leader>e")
+  vim.keymap.del("n", "<tab>")
+end)
+
 
