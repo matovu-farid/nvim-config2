c359974ed5dbcd4888e623237711d30296af02eb
matovu-farid <matovu90@gmail.com>
2024-07-16 00:39:43 +0300

    Add solargraph

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -4,8 +4,7 @@ local on_init = require("nvchad.configs.lspconfig").on_init
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls" ,"tailwindcss"}
-
+local servers = { "html", "cssls" ,"tailwindcss", "ruby_lsp", "solargraph"}
 -- print(vim.inspect(capabilities))
 
 -- lsps with default config
@@ -22,3 +21,36 @@ end
 --   on_init = on_init,
 --   capabilities = capabilities,
 -- }
+local function add_ruby_deps_command(client, bufnr)
+  vim.api.nvim_buf_create_user_command(bufnr, "ShowRubyDeps", function(opts)
+    local params = vim.lsp.util.make_text_document_params()
+    local showAll = opts.args == "all"
+
+    client.request("rubyLsp/workspace/dependencies", params, function(error, result)
+      if error then
+        print("Error showing deps: " .. error)
+        return
+      end
+
+      local qf_list = {}
+      for _, item in ipairs(result) do
+        if showAll or item.dependency then
+          table.insert(qf_list, {
+            text = string.format("%s (%s) - %s", item.name, item.version, item.dependency),
+            filename = item.path
+          })
+        end
+      end
+
+      vim.fn.setqflist(qf_list)
+      vim.cmd('copen')
+    end, bufnr)
+  end,
+  {nargs = "?", complete = function() return {"all"} end})
+end
+
+require("lspconfig").ruby_lsp.setup({
+  on_attach = function(client, buffer)
+    add_ruby_deps_command(client, buffer)
+  end,
+})
